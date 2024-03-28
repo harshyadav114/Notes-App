cc882d7d20b4eb230b38eb5aa5dc9e548bf2ed59
harsh yadav <harshyadav0773@gmail.com>
2024-03-28 14:33:14 +0530

    initial commit

---
+++ mynotes/urls.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+from django.contrib import admin
+from django.urls import path,include
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/',include('api.urls')),
+    path('',TemplateView.as_view(template_name='index.html'))
+]
